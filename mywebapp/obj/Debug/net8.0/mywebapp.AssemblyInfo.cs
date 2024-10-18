f77d68a20e94e70e6326a4cca68560a434bb0461
ASUS <aslihantannriverdi0502@gmail.com>
2024-10-18 11:18:44 +0300

    Updated index.html with price numbers

--- mywebapp/obj/Debug/net8.0/mywebapp.AssemblyInfo.cs
+++ mywebapp/obj/Debug/net8.0/mywebapp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mywebapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff3d8428526369fe0f88afd3ce1f235961bd540b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e0549a7da908da59e23400c218cd38ac46c6743")]
 [assembly: System.Reflection.AssemblyProductAttribute("mywebapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mywebapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
